5fb70f4812a7a6f46b563c2221f6796c3549281c
IvetaDuda <92565279+IvetaDuda@users.noreply.github.com>
2022-04-30 13:22:07 +0300

    fixed task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,42 +1,71 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+// const galleryContainerEl = document.querySelector('.gallery');
+
+// function galleryItemsEl(list) {
+//   return list
+//     .map(
+//       ({ preview, original, description }) =>
+//         `<a class="gallery__item" href="${original}">
+//           <img class="gallery__image"
+//           src="${preview}"
+//           alt="${description}" />
+//         </a>`
+//     )
+//     .join('');
+// }
+
+// galleryContainerEl.addEventListener('click', openModalGallery);
+
+// function openModalGallery(event) {
+//   event.preventDefault();
+//   if (event.target.nodeName !== 'IMG') {
+//     return;
+//   }
+
+//   let gallery = new SimpleLightbox('.gallery a', {
+//     captionsData: `alt`,
+//     captionDelay: 250,
+//   });
+
+//   galleryContainerEl.addEventListener('keydown', (event) => {
+//     if (event.key === 'Escape') {
+//       gallery.close();
+//     }
+//   });
+//   // galleryContainerEl.addEventListener('click', (event) => {
+//   //   if (event.currentTarget.nodeName !== 'IMG') {
+//   //     gallery.currentTarget.close();
+//   //   }
+//   // });
+// }
+
+// galleryContainerEl.insertAdjacentHTML(
+//   'beforeend',
+//   galleryItemsEl(galleryItems)
+// );
+
 const galleryContainerEl = document.querySelector('.gallery');
-
-function galleryItemsEl(list) {
-  return list
-    .map(
-      ({ preview, original, description }) =>
-        `<a class="gallery__item" href="${original}">
-          <img class="gallery__image"
-          src="${preview}" 
-          alt="${description}" />
-        </a>`
-    )
-    .join('');
-}
-
-galleryContainerEl.addEventListener('click', openModalGallery);
-
-function openModalGallery(event) {
-  event.preventDefault();
-  if (event.target.nodeName !== 'IMG') {
-    return;
-  }
-
-  let gallery = new SimpleLightbox('.gallery a', {
-    captionsData: `alt`,
-    captionDelay: 250,
-  });
-
-  galleryContainerEl.addEventListener('keydown', (event) => {
-    if (event.key === 'Escape') {
-      gallery.close();
-    }
-  });
-}
-
 galleryContainerEl.insertAdjacentHTML(
   'beforeend',
   galleryItemsEl(galleryItems)
 );
+function galleryItemsEl(item) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          alt="${description}"
+        />
+      </a>`;
+    })
+    .join('');
+}
+
+const gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
